62062f9661fc83f974bf33396a0d5d12855a25de
Alexis Fournier <alexis.frnr62@gmail.com>
2020-03-22 15:16:45 +0100

    created skull, need to implement CNN

--- api/images/urls.py
+++ api/images/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OneImageView, ImagesView
+from .views import *
 
 urlpatterns = [
     path('images', ImagesView.as_view()),
-    path('images/<int:pk>', OneImageView.as_view())
+    path('images/<int:pk>', OneImageView.as_view()),
+    path('images/search/<int:pk>', SearchView.as_view()),
 ]
