a6623851b39a5c7eb6a07b6cfb7372f06ec2023b
Ryan Verlande <ryan.verlande@leroymerlin.fr>
2020-04-04 15:35:03 +0200

    feat : Added CNN feature in Django Project

--- api/images/serializers.py
+++ api/images/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Image, ImageResult, ImageSearch
+from .models import Image, ImageResult
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,9 +10,4 @@ class ImageSerializer(serializers.ModelSerializer):
 class ImageResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImageResult
-        fields = '__all__'
-
-class ImageSearchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ImageSearch
         fields = '__all__'
--- api/images/urls.py
+++ api/images/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('images', ImagesView.as_view()),
-    path('images/<int:pk>', OneImageView.as_view()),
-    path('images/search/<int:pk>', SearchView.as_view()),
+    path('images/', ImagesView.as_view()), # toutes les images uploadée
+    path('images/<int:pk>/', OneImageView.as_view()), # une image uploadée identifiée
+    path('images/result/', ImagesResultView.as_view()), # top 5 images du CNN
 ]
